dd2222a7c21fb1d9381916ee69216c6bd45e17a1
Schneuzi <mike.mueller28@outlook.com>
2025-05-22 20:30:03 +0200

    adds src directory with a header.php and index.php for a first basis

---
+++ src/header.php
@@ -0,0 +1,10 @@
+<?php?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>GeekBase</title>
+</head>
+
+</html>
---
+++ src/index.php
@@ -0,0 +1,4 @@
+<?php include 'header.php' ?>
+
+<h1>GeekBase</h1>
+<p>This is the first paragraph on my experimental Website which is written in HTML and uses PHP as a Backend.</p>
