242a88ceba809cd87dd1628bbaec2d9c989aaf7e
Schneuzi <mike.mueller28@outlook.com>
2025-05-22 23:22:44 +0200

    creates a styles.css to be used for formatting

--- src/header.php
+++ src/header.php
@@ -1,10 +1,10 @@
-<?php?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <title>GeekBase</title>
+    <link rel="stylesheet" href="styles.css"
 </head>
 
-</html>
+<body>
